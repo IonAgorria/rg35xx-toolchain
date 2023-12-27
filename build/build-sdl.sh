94b38535f0b5e7374f0da10f3923e84b7504f866
Ion Agorria <ion@agorria.com>
2023-12-28 00:25:15 +0100

    Source envs instead

--- build/build-sdl.sh
+++ build/build-sdl.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-PATH="${PATH}:/opt/miyoo/bin:/opt/miyoo/arm-miyoo-linux-uclibcgnueabi/sysroot/usr/bin"
-CROSS_COMPILE=/opt/miyoo/bin/arm-miyoo-linux-uclibcgnueabi-
-CC=${CROSS_COMPILE}gcc
-AR=${CROSS_COMPILE}ar
-AS=${CROSS_COMPILE}as
-LD=${CROSS_COMPILE}ld
-CXX=${CROSS_COMPILE}g++
-HOST=arm-miyoo-linux-uclibcgnueabi
-PREFIX=/opt/miyoo/
+
+source ./setup-env.sh
 
 git clone -b release-1.2.15 https://github.com/libsdl-org/SDL-1.2.git
 cd SDL-1.2 
